3c8051080c7bfe24c81b16e58a44bc4deafc8af1
akkie00 <akshay8801.ak@gmail.com>
2018-05-19 23:28:34 +0530

    started working on titleCase FCC algo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # JavaScript-basics
+
+This repo is all about storing information which I would be learning in JavaScript
+
+It will mostly be updated whenever I learn something new. 
+
--- Strings/FCC/titleCase.js
+++ Strings/FCC/titleCase.js
@@ -1,13 +1,20 @@
 function titleCase (str){
 var lowStr = str.toLowerCase();
 var splitStr = lowStr.split(' ');
-console.log(splitStr);
+// console.log(splitStr);
 
 
-splitStr.forEach(word => {
-    
-});
+// splitStr.forEach(word => {
+//     const titleLetters = word.charAt(0).toUpperCase();
+//     const titledSentence = word.replace(word.charAt(0), titleLetters);
+//     // return titledSentence;
+//     console.log(titledSentence);
+// });
 
+for(var i =0; i< splitStr.length; i++){
+    const titleLetters = splitStr[i].charAt(0).toUpperCase();
+    console.log(titleLetters);
+}
 
 }
 
