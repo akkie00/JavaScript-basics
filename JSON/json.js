3a76ffda502356942a00b6c083b66e1269e17299
akkie00 <akshay8801.ak@gmail.com>
2018-08-11 15:36:40 +0530

    JSON, localStorage and set and Get

---
+++ JSON/json.js
@@ -0,0 +1,11 @@
+let exampleJSON = JSON.stringify({boo : true, name: "Dylan", id: 123});
+
+console.log(typeof exampleJSON);
+
+console.log(exampleJSON);
+
+exampleJSON = JSON.parse(exampleJSON);
+    console.log(typeof exampleJSON)
+    console.log(exampleJSON)
+
+
